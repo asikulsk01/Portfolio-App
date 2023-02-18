eb84928b03f21fadb8c4fd151601868dff37a963
ashikulsk01 <ashikulsk.01@gmail.com>
2023-02-19 00:00:57 +0530

    New updates

---
+++ app/src/main/java/com/example/scrollview/webViewGithub.kt
@@ -0,0 +1,11 @@
+package com.example.scrollview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class webViewGithub : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view_github)
+    }
+}
