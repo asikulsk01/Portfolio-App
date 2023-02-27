e70f8a6a74aadbc96de8bc2e4000b980da0caa0c
ashikulsk01 <ashikulsk.01@gmail.com>
2023-02-27 23:32:29 +0530

    added web view

--- app/src/main/java/com/example/scrollview/MainActivity.kt
+++ app/src/main/java/com/example/scrollview/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.scrollview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnGithub= findViewById<ImageView>(R.id.imgGithub)
+
+
+        btnGithub.setOnClickListener{
+            intent= Intent(applicationContext,webViewGithub::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/scrollview/webViewGithub.kt
+++ app/src/main/java/com/example/scrollview/webViewGithub.kt
@@ -1,11 +1,32 @@
 package com.example.scrollview
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
 
 class webViewGithub : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view_github)
+
+        val myWebViewLayout= findViewById<WebView>(R.id.webViewMygithubPage)
+
+        webViewSetup(myWebViewLayout)
     }
+
+    //Method define
+   @RequiresApi(Build.VERSION_CODES.O)
+   private fun webViewSetup(webview: WebView){
+       webview.webViewClient= WebViewClient()
+        webview.apply {
+            settings.javaScriptEnabled= true
+            settings.safeBrowsingEnabled= true
+            loadUrl("https://github.com/asikulsk01")
+        }
+   }
 }
